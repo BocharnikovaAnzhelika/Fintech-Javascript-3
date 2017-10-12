174dfa6078d9e213f4729d1424dc6847160c6146
Anzhelika Bocharnikova <bocharnikovaanzhelika@gmail.com>
2017-10-12 17:27:54 +0300

    lesson-03-ver-01

--- 03/2/task.js
+++ 03/2/task.js
@@ -1,7 +1,9 @@
+/* eslint-disable linebreak-style */
 /**
 * Сделать функцию, которая reject'ит возвращаемый промис, передавая в качестве ошибки строку 'timeout_error',
 * если он не resolve'ится за указанный timeout, или ведет себя эквивалентно исходному.
 * В учебных целях для этой задачи просьба не использовать Promise.race.
+// eslint-disable-next-line linebreak-style
 *
 * @param {Promise} promise исходный промис
 * @param {Number} timeoutInMilliseconds время для timeout в миллисекундах
